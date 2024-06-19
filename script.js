bf9204ec4e9b4d0fc875a043049a772d7539bacc
Joaquin <joaquinbrusco1@gmail.com>
2024-06-19 20:17:29 -0300

    First commit

---
+++ script.js
@@ -0,0 +1,144 @@
+const drinks = [
+    {
+        name: "Machiattos",
+        drinks: [
+            {
+                id: "Machiattos0",
+                name: "Latte Machiatto",
+                image: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FLatte%20Macchiato%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop 1x, https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FLatte%20Macchiato%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop&dpr=2 2x",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+                price: "2.00$",
+            },
+            {
+                id: "Machiattos1",
+                name: "Skinny Caramel Machiatto",
+                image: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FCaramel%20Macchiato%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop 1x, https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FCaramel%20Macchiato%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop&dpr=2 2x",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+                price: "2.00$",
+            },
+            {
+                id: "Machiattos2",
+                name: "Caramel Machiatto",
+                image: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FCaramel%20Macchiato%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop 1x, https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FCaramel%20Macchiato%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop&dpr=2 2x",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+                price: "3.99$",
+            }
+        ]
+    },
+    {
+        name: "Cappucinos",
+        drinks: [
+            {
+                id: "Cappucinos0",
+                name: "Cappucino",
+                image: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FCappuccino%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop 1x, https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FCappuccino%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop&dpr=2 2x",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+                price: "0.99$",
+            }
+        ]
+    },
+    {
+        name: "Americanos",
+        drinks: [
+            {
+                id: "Americanos0",
+                name: "Americano",
+                image: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FAmericano%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop 1x, https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FAmericano%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop&dpr=2 2x",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+                price: "1.99$",
+            }
+        ]
+    },
+    {
+        name: "Mochas",
+        drinks: [
+            {
+                id: "Mochas0",
+                name: "White Mocha",
+                image: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FMocha%20blanco%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop 1x, https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FMocha%20blanco%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop&dpr=2 2x",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+                price: "3.00$",
+            },            
+            {
+                id: "Mochas1",
+                name: "Mocha",
+                image: "https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FMocha%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop 1x, https://djftrby1k8irl.cloudfront.net/s3fs-public/2022-03%2FMocha%20425x425%20sin%20fondo.png?auto=format,compress&q=70&crop=focalpoint&ar=1:1.0&w=180&fit=crop&dpr=2 2x",
+                description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation",
+                price: "3.99$",
+            }
+        ]
+    }
+]
+
+const drinksListContainer = document.querySelector(".drink-list-container");
+
+drinks.forEach(drink => {
+    drinksListContainer.innerHTML += `
+        <h3>
+                ${drink.name}
+        </h3>
+        <section>
+            ${drink.drinks.map((drink, i) => `
+                <div onclick="openDrink('${drink.id}')" id = "${drink.name.replaceAll(" ", "-") + i}">
+                    <img height="180" width="180" src="${drink.image}" alt="${drink.name}">
+                    <h4>${drink.name}</h4>
+                </div>
+            `).join("")}
+        </section>
+    `;
+});
+
+
+function openDrink(id) {
+    const drinkDialog = document.querySelector(".drink-dialog");
+    let selectedDrink = null;
+
+    drinks.forEach(drink => {
+        drink.drinks.forEach(subDrink => {
+            if (subDrink.id === id) {
+                selectedDrink = subDrink;
+            }
+        });
+    });
+
+    if (selectedDrink) {
+        drinkDialog.querySelector(".drink-dialog-content").innerHTML = `
+            <img height="350" width="350" src="${selectedDrink.image}" alt="">
+            <span class="drink-dialog-content-span">
+                <h3>${selectedDrink.name} <span class="drink-price">${selectedDrink.price}</span></h3>
+                <p>${selectedDrink.description}</p>
+                
+                <section style="display: flex; gap: 20px;">
+                    <button><span class="material-symbols-outlined">shopping_cart</span>Add to order</button>
+                    <button><span class="material-symbols-outlined">favorite</span></button> 
+                </section>
+            </span>
+        `;
+
+        document.body.style.overflow = "hidden";
+        drinkDialog.style.display = "flex";
+        drinkDialog.style.opacity = "0";
+        setTimeout(() => {
+            drinkDialog.style.opacity = "1";
+        }, 200);
+        document.querySelector(".drink-dialog-content > span").style.display = "none";
+        document.querySelector(".drink-dialog-content > span").style.opacity = "0";
+        document.querySelector(".drink-dialog-content").style.borderRadius = "20px";
+
+        setTimeout(() => {
+            document.querySelector(".drink-dialog-content > span").style.display = "flex";
+            document.querySelector(".drink-dialog-content").style.borderRadius = "0";
+            document.querySelector(".drink-dialog-content span button:first-child").style.display = "flex";
+            document.querySelector(".drink-dialog-content span button:last-child").style.display = "flex";
+            setTimeout(() => {
+                document.querySelector(".drink-dialog-content > span").style.opacity = "1";
+            }, 200);
+        }, 1000);
+    }
+}
+
+function closeDrink() {
+    const drinkDialog = document.querySelector(".drink-dialog");
+    drinkDialog.style.display = "none";
+    document.body.style.overflow = "auto";
+}
